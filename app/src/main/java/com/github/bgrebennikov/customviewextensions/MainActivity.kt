a1d5df91d755dcab3a88a5549ef59ec4ca8d703a
bgrebennikov <bgrebennikovv@gmail.com>
2022-04-07 15:26:08 +0400

    Fragment container view

--- app/src/main/java/com/github/bgrebennikov/customviewextensions/MainActivity.kt
+++ app/src/main/java/com/github/bgrebennikov/customviewextensions/MainActivity.kt
@@ -2,10 +2,24 @@ package com.github.bgrebennikov.customviewextensions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.bgrebennikov.customviewextensions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerMain, MainFragment.newInstance())
+                .commit()
+        }
+
+
     }
 }
